b91f6025cd61eac2cbd78d8814ef8b28676c8653
AlejandroCiuba <alc307@pitt.edu>
2021-09-15 19:08:05 -0700

    Stuck on makefile

--- lab02/test.c
+++ lab02/test.c
@@ -9,9 +9,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "matrix library/matrix_def.h" //Defines matrix and vector, as well as error codes
-#include "matrix library/matrix_ops.h" //Defines matrix and vector operations
-#include "matrix library/matrix_utility.h" //Defines utility functions for debugging and convenience
+#include "matrix_library/matrix_def.h" //Defines matrix and vector, as well as error codes
+#include "matrix_library/matrix_ops.h" //Defines matrix and vector operations
+#include "matrix_library/matrix_utility.h" //Defines utility functions for debugging and convenience
 
 int main() {
 
